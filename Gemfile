5e24f0c6af09108601a0c5a20aa10cd8fdbccf85
Rob Durst <me@robdurst.com>
2024-08-23 20:05:39 -0600

    add github info for project

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@ ruby '3.0.0'
 
 source 'https://rubygems.org'
 
+gem 'git'
 gem 'json'
+gem 'rackup', '~> 2.1'
 gem 'sinatra'
 gem 'sinatra-contrib'
-
-gem 'rackup', '~> 2.1'
--- app.rb
+++ app.rb
@@ -1,7 +1,27 @@
-# app.rb
-
 require 'sinatra'
+require 'net/http'
+require 'json'
+require 'git'
 
 get '/' do
+  repo = 'spaced-out-thoughts-dev-foundation/don_control_hub'
+  branch = 'main'
+
+  # GitHub API endpoint to fetch the latest commit
+  url = "https://api.github.com/repos/#{repo}/commits/#{branch}"
+
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  commit = JSON.parse(response)
+
+  @latest_commit_sha = commit['sha']
+  @latest_commit_message = commit['commit']['message']
+
+  repo_path = File.expand_path('~/spaced-out-thoughts-dev-foundation/don_control_hub')
+  g = Git.open(repo_path)
+
+  @latest_commit_sha_local = g.object('HEAD').sha
+  @latest_commit_message_local = g.object('HEAD').message
+
   erb :index
 end
