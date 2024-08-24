8f0096d9bd2afb59f4116a3d1a3c6177f87853ab
Rob Durst <me@robdurst.com>
2024-08-24 10:52:51 -0600

    fix diagnostics view

--- app.rb
+++ app.rb
@@ -22,3 +22,40 @@ get '/' do
 
   erb :index
 end
+
+get '/diagnostics' do
+  @version = DONControlHub::VERSION
+  erb :diagnostics
+end
+
+def cpu_usage
+  top_output = `top -bn1 | grep "Cpu(s)"`
+  cpu_idle = top_output.match(/id,\s*(\d+\.\d+)/)[1].to_f
+  100.0 - cpu_idle
+end
+
+def memory_usage
+  meminfo = File.read('/proc/meminfo')
+  total_memory = meminfo.match(/MemTotal:\s+(\d+)/)[1].to_i
+  free_memory = meminfo.match(/MemFree:\s+(\d+)/)[1].to_i
+  used_memory = total_memory - free_memory
+  (used_memory.to_f / total_memory * 100).round(2)
+end
+
+def disk_usage
+  df_output = `df / | grep -v Filesystem`
+  usage_percentage = df_output.match(/\s+(\d+)%/)[1].to_i
+end
+
+get '/data' do
+  content_type :json
+
+  # Generate dummy data for demonstration; replace with actual data collection
+  data = {
+    cpu: cpu_usage,
+    disk: memory_usage,
+    memory: disk_usage
+  }
+
+  data.to_json
+end
